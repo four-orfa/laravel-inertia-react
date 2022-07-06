eb4d56ea437f7d53cecc6e8928ef8fba8addb378
Toshinori Sumida <alias.ceuta@gmail.com>
2022-07-07 04:10:49 +0900

    laravel/breeze docker exec 9f99c8ec2713 composer require laravel/breeze --dev ./vendor/bin/sail artisan breeze:install react

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Scripts -->
+    @routes
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
 
-<body>
+<body class="font-sans antialiased">
     @inertia
 </body>
 
